64f5e2d83be83abe142c8ccd2841b01d14abdb9e
radox9 <grzywaczra@gmail.com>
2024-04-08 16:59:48 +0200

    Initial commit

---
+++ API/Program.cs
@@ -0,0 +1,20 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
